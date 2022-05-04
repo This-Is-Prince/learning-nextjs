95d846e0d054c49fcc546fe9a180ca522047bd8d
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-05-04 14:21:27 +0530

    adding blog jsons as data chapter 16 completed

--- code_with_harry/huntingcoder/pages/blog.js
+++ code_with_harry/huntingcoder/pages/blog.js
@@ -2,28 +2,31 @@ import React from "react";
 import styles from "../styles/Blog.module.css";
 import Link from "next/link";
 
+// Step 1: Collect all the files from blogdata directory.
+// Step 2: Iterate through them and Display them.
+
 const Blog = () => {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <div>
           <Link href={"/blogpost/learn-javascript"}>
-            <h3 className={styles.blogItemh3}>
+            <h2 className={styles.blogItemh3}>
               How to learn JavaScript in 2022?
-            </h3>
+            </h2>
           </Link>
           <p>JavaScript is the language used to design logic for the web.</p>
         </div>
         <div>
-          <h3 className={styles.blogItemh3}>
+          <h2 className={styles.blogItemh3}>
             How to learn JavaScript in 2022?
-          </h3>
+          </h2>
           <p>JavaScript is the language used to design logic for the web.</p>
         </div>
         <div>
-          <h3 className={styles.blogItemh3}>
+          <h2 className={styles.blogItemh3}>
             How to learn JavaScript in 2022?
-          </h3>
+          </h2>
           <p>JavaScript is the language used to design logic for the web.</p>
         </div>
       </main>
